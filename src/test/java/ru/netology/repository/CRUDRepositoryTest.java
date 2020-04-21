de994c19ac64172966fae3bee7f59b197e61e16e
vika <v.sapunkova@neurobotics.ru>
2020-04-21 16:36:14 +0300

    Initial commit

---
+++ src/test/java/ru/netology/repository/CRUDRepositoryTest.java
@@ -0,0 +1,17 @@
+package ru.netology.repository;
+
+import org.junit.jupiter.api.Nested;
+
+public class CRUDRepositoryTest {
+    @Nested
+    public class Empty {
+    }
+
+    @Nested
+    public class SingleItem {
+    }
+
+    @Nested
+    public class MultipleItems {
+    }
+}
